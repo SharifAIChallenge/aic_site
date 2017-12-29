a38b997f6da4133ccdbc7501aa74d3735e62bff4
faryabimm <faryabimohammadmahdi@gmail.com>
2017-12-29 11:15:32 +0330

    added travis and db  configuration in settings.py

--- aic_site/settings.py
+++ aic_site/settings.py
@@ -12,6 +12,22 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+##################################################################
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
+
+##################################################################
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
