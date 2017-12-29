934ff09b676758b359970bab4bfb454097e37c5e
faryabimm <faryabimohammadmahdi@gmail.com>
2017-12-29 20:45:35 +0330

    working on travis CI configuration
    added bundle and deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+
+# Extract the package
+
+tar -xzf deploy_package.tgz
+rm deploy_package.tgz
+
+# Copy any file we want to keep from build to build
+
+
+
+# Swap it all around, keeping the previous version aside in case something goes wrong
+
+rm -rf www_old
+mv www www_old
+mv build www
