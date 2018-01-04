fd94b6184f5d9c7a82686f209c937687e2724d09
Alireza <alirezakhoshghalb@ymail.com>
2018-01-04 21:14:59 +0330

    Change game models max_lengths

--- apps/game/models/game.py
+++ apps/game/models/game.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Game(models.Model):
-    name = models.CharField(max_length=255)
-    infra_token = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=256)
+    infra_token = models.CharField(max_length=256, unique=True)
 
