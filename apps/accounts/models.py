927e84b80034b39eb2951329e19f5c1ffb334a4a
Amin <Amin@amins-MacBook-Pro-2.local>
2018-01-10 21:10:10 +0330

    testing test

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Q
+
+from apps.game.models import Match
 
 
 class Profile(models.Model):
@@ -15,6 +18,17 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_competition_matches(self, competition_id):
+        matches = Match.objects.filter(Q(part1__object_id=self.id) | Q(part2__object_id=self.id),
+                                       competition=competition_id)
+        return matches
+        # challenges = self.challanges
+        # for challenge in challenges:
+        #     for competition in challenge.competitions:
+        #         if competition.id == competition_id:
+        #             matches.extend(competition.match_set)
+        # return matches[start:start + 5]
 
 
 class UserParticipatesOnTeam(models.Model):
--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,90 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
-# Create your tests here.
+from apps.accounts.models import Team
+from apps.game.models import Challenge, Competition, Match
+
+
+def populate_users():
+    for i in range(9):
+        user = User()
+        user.username = str(i) + "DummyCamelCaseTeamForTest"
+        user.save()
+
+
+def populate_teams():
+    users = User.objects.all()
+    team_users = []
+    for i in range(len(users)):
+        team_users += users[i]
+        if len(team_users) == 3:
+            team = Team()
+            team.name = i / 3 + 1
+            team.participants = team_users
+            team.save()
+            team_users = []
+
+
+def populate_challenges():
+    challenge = Challenge()
+    challenge.title = "Dummiest Challenge created ever"
+    challenge.save()
+
+
+def populate_competitions():
+    challenge = Challenge.objects.all()[0]
+    types = ['elim', 'league', 'double']
+    for i in range(3):
+        competition = Competition()
+        competition.type = types[i]
+        competition.challenge = challenge
+        competition.save()
+
+
+class TestTeam(TestCase):
+    def setUp(self):
+        super().setUp()
+        populate_users()
+        populate_teams()
+        populate_challenges()
+        populate_competitions()
+
+    def test_get_team_matches(self):
+        teams = Team.objects.all()
+        competition = Competition.objects.get(type='elim')
+        challenge = Challenge.objects.all()[0]
+        for team in teams:
+            team.challanges = [challenge]
+            team.save()
+
+        match1 = Match()
+        match2 = Match()
+        match3 = Match()
+
+        matches = [match1, match2, match3]
+
+        match1.part1.content_type = ContentType.objects.get_for_model(Team)
+        match1.part2.content_type = ContentType.objects.get_for_model(Team)
+        match2.part1.content_type = ContentType.objects.get_for_model(Team)
+        match2.part2.content_type = ContentType.objects.get_for_model(Team)
+        match3.part1.content_type = ContentType.objects.get_for_model(Team)
+        match3.part2.content_type = ContentType.objects.get_for_model(Team)
+
+        match1.part1.object_id = teams[0].id
+        match1.part2.object_id = teams[1].id
+        match2.part1.object_id = teams[0].id
+        match2.part2.object_id = teams[2].id
+        match3.part1.object_id = teams[1].id
+        match3.part2.object_id = teams[2].id
+
+        competition.match_set = [match1, match2, match3]
+
+        challenge.competitions = [competition]
+
+        for match in matches:
+            match.save()
+
+        team0_matches = teams[0].get_competition_matches(competition.id)
+
+        self.assertEqual(len(team0_matches), 2)
