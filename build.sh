de6b052892a0589776cc36f7133fa305ea6d9bc7
faryabimm <faryabimohammadmahdi@gmail.com>
2018-01-10 23:16:07 +0330

    working on travis CI and ssh integration

--- build.sh
+++ build.sh
@@ -8,21 +8,31 @@ DEPLOY_HOST=81.31.168.207
 LINE='###############################################################################################################'
 
 
-echo $LINE; echo '~~~~~>   CREATING DEPLOYMENT PACKAGE'; echo
+echo $LINE; echo '~~~~~>   GATHERING DEPLOYMENT FILES'; echo
 mkdir build
 rsync -r --exclude='.*' --exclude='build' ./ build
 cd build/config/nginx/sites-available
 cp aichallenge.sharif.edu ../sites-enabled
 cd ../../../..
+echo '~~~~~>   DONE'
+echo $LINE; echo '~~~~~>   OPTIMIZING IMAGES FOR DEPLOYMENT'; echo
+cd build/apps/
+find . -iname '*.png' -print0 | xargs -0 optipng -o7 -preserve
+find . -iname '*.jpg' -print0 | xargs -0 jpegoptim --max=90 --strip-all --preserve --totals
+cd ../..
+echo '~~~~~>   DONE'
+echo $LINE; echo '~~~~~>   COMPRESSING DEPLOYMENT PACKAGE'; echo
 tar -czf deploy_package.tgz build
 echo '~~~~~>   DONE'
 echo $LINE; echo '~~~~~>   UPLOADING DEPLOYMENT PACKAGE'; echo
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o stricthostkeychecking=no deploy_package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e rsync --progress -avzhe 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' \
+ deploy_package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 rm -r build deploy_package.tgz
 echo '~~~~~>   DONE'
 echo $LINE; echo '~~~~~>   UPLOADING DEPLOYMENT SCRIPT'; echo
-sshpass -e scp -o stricthostkeychecking=no deploy.sh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e rsync --progress -avzhe 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' \
+ deploy.sh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 echo '~~~~~>   DONE'
 echo $LINE; echo '~~~~~>   DEPLOYING...'; echo
 sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST chmod +x $DEPLOY_PATH/deploy.sh
