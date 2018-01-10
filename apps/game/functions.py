65174c48ee4b3c4a6573effee8142eda3c139423
sadegh <smahdavi4@gmail.com>
2018-01-10 13:36:37 +0330

    Updated functions.py

--- apps/game/functions.py
+++ apps/game/functions.py
@@ -1,48 +1,109 @@
+import random
+import string
 
 
-def upload_file(file, lang, game_id):
-
-    # Tell infrastructure to upload and compile file with language lang and game id game_id
-    # Returns the token of the submission.
-    pass
+def random_token():
+    chars = string.ascii_letters + string.digits
+    return ''.join((random.choice(chars)) for i in range(15))
 
 
-def compile_submissions(submission_tokens):
+def upload_file(file):
+    # Tell infrastructure to upload file.
+    # Returns the token of the uploaded file.
 
-    # Tell the infrastructure to compile a list of submissions (probably again)
-
-    pass
-
-
-def download_log(log_token):
-
-    # Download the log file of a match (with matches' token) from the infrastructure.
-    # Returns the log file.
-
-    pass
+    # Request to upload file
+    return random_token()
 
 
-def run_matches(matches):
+def download_file(file_token):
+    # Download the file of a match (with file's) from the infrastructure.
+    # Returns the file.
 
+    file = open('static/play_logs/' + file_token + '.log', 'w')
+    # Request and get the log file
+    file.write("Mock Log File")
+    file.close()
+
+
+def compile_submissions(submission_tokens, game_id):
+    # Tell the infrastructure to compile a list of submission
+
+    submits = []
+    for submission_token in submission_tokens:
+        submits.append({
+            "game": game_id,
+            "section": "compile",
+            "parameters": {
+                "string_parameter1": "parameter1_value",
+                "string_parameter2": "parameter2_value",
+                "file_parameter1": "file_parameter1_token"
+            }
+        })
+
+    # Send request to infrastructure to compile them
+    # TODO : A thread to call compile results
+
+    compile_tokens = []  # Get the array from the infrastructure.
+
+    for x in submission_tokens:
+        compile_tokens.append(random_token())
+
+    return compile_tokens
+
+
+def run_matches(matches, game_id):
     # Tell the infrastructure to run a list of matches (match includes tokens,maps,...)
     # Returns the list of tokens assigned to the matches
 
+    games = []
+    for submission_token in matches:
+        games.append({
+            "game": game_id,
+            "section": "play",
+            "parameters": {
+                "string_parameter1": "parameter1_value",
+                "string_parameter2": "parameter2_value",
+                "file_parameter1": "file_parameter1_token"
+            }
+        })
+
+    # Send request to infrastructure to compile them
+    # TODO : A thread to call matches results
+
+    match_tokens = []  # Get the array from the infrastructure.
+
+    for x in matches:
+        match_tokens.append(random_token())
+
+    return match_tokens
+
+
+def compilation_results(compile_file):
+    # Returns compilation results.
+
+    # TODO
+
     pass
 
 
-""" APIS : """
+def matches_results(matches):
+    # Return matches results.
 
-
-def compile_response(compile_results):
-
-    # An API for infrastructure to tell us the compilation results
-    # Returns compilation statuses.
+    # TODO
 
     pass
 
 
-def matches_results(matches_results):
+def pull_results():
+    # Requests latest results from the infrastructure and updates them
 
-    # An API for infrastructure to tell us the matches results.
+    submits = []
+    matches = []
 
-    pass
+    games = []  # Request updates from the infrastructure.
+
+    for game in games:
+        if game["section"] == "compile":
+            compilation_results(game)
+        elif game["section"] == "play":
+            matches_results(game)
