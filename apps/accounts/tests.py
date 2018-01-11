e7c6288861f3dcb48df8a557f462490915fc26bb
ehsan <soltanaghaei97@gmail.com>
2018-01-11 06:36:27 +0330

    game scheduling and test

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,3 +1,102 @@
+import datetime
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
-# Create your tests here.
+from apps.accounts.models import Team, UserParticipatesOnTeam
+from apps.game.models import Challenge, Competition, Match, Game, Participant
+from apps.game.models.challenge import TeamParticipatesChallenge
+
+
+def populate_users():
+    for i in range(9):
+        user = User()
+        user.username = str(i) + "DummyCamelCaseTeamForTest"
+        user.save()
+
+
+def populate_teams():
+    users = User.objects.all()
+    i = 0
+    team = None
+    for user in users:
+        if i % 3 == 0:
+            team = Team()
+            team.name = i / 3 + 1
+            team.save()
+        participation = UserParticipatesOnTeam()
+        participation.user = user
+        participation.team = team
+        participation.save()
+        i += 1
+
+
+def populate_challenges():
+    challenge = Challenge()
+    challenge.title = "Dummiest Challenge created ever"
+    challenge.start_time = timezone.now()
+    challenge.end_time = timezone.now() + datetime.timedelta(days=1)
+    challenge.registration_start_time = challenge.start_time
+    challenge.registration_end_time = challenge.end_time
+    challenge.registration_open = True
+    challenge.team_size = 3
+    challenge.entrance_price = 1000
+    game = Game()
+    game.name = "AIC 2018"
+    game.save()
+    challenge.game = game
+    challenge.save()
+
+
+def populate_competitions():
+    challenge = Challenge.objects.all()[0]
+    types = ['elim', 'league', 'double']
+    for i in range(3):
+        competition = Competition()
+        competition.type = types[i]
+        competition.challenge = challenge
+        competition.save()
+
+
+class TestTeam(TestCase):
+    def setUp(self):
+        super().setUp()
+        populate_users()
+        populate_teams()
+        populate_challenges()
+        populate_competitions()
+
+    def test_get_team_matches(self):
+        teams = Team.objects.all()
+        competition = Competition.objects.get(type='elim')
+        challenge = Challenge.objects.all()[0]
+        team_participation = []
+        for team in teams:
+            participation = TeamParticipatesChallenge()
+            participation.team = team
+            participation.challenge = challenge
+            participation.save()
+            team_participation.append(participation)
+
+        matches = []
+        for i in range(3):
+            matches.append(Match())
+            participants = []
+            for j in range(2):
+                participant = Participant()
+                participant.depend = team_participation[(i + j) % 3]
+                participant.save()
+                participants.append(participant)
+            matches[i].part1 = participants[0]
+            matches[i].part2 = participants[1]
+            matches[i].competition = competition
+            matches[i].save()
+
+        challenge.competitions = [competition]
+        challenge.save()
+
+        team0_matches = teams[0].get_competition_matches(competition.id)
+
+        self.assertEqual(len(team0_matches), 2)
--- apps/game/models/competition.py
+++ apps/game/models/competition.py
@@ -2,7 +2,9 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from .challenge import Challenge, TeamSubmission
+
+from apps.accounts.models import Team
+from .challenge import Challenge, TeamSubmission, TeamParticipatesChallenge
 
 
 class Competition(models.Model):
@@ -15,15 +17,115 @@ class Competition(models.Model):
     challenge = models.ForeignKey(Challenge, related_name='competitions')
     type = models.CharField(max_length=128, choices=TYPE_CHOICES)
 
+    def __init__(self, teams, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.type == 'league':
+            Competition.create_new_league(self, teams)
+        if self.type == 'double':
+            Competition.create_new_double_elimination(self, teams)
+
+    def create_new_league(self, teams): # algorithm for scheduling is Round-robin tournament
+        matches = []
+        bye = None # TODO: replace with the right object
+        if len(teams) % 2 == 1:
+            teams.append(bye)
+        # random_shuffle on teams
+        first_part = teams[0:len(teams)/2]
+        second_part = teams[len(teams)/2:len(teams)]
+        for round in range(2):
+            for week in range(len(teams)-1):
+                for i in range(len(teams)/2):
+                    # matches.append(Match(self, first_part[i], second_part[i]))
+                    matches.append(
+                        Match(
+                            competition=self,
+                            part1=Participant(
+                                depend=TeamParticipatesChallenge.objects.get(team=first_part[i],
+                                                                             challenge=self.challenge),
+                                depend_method='itself'),
+                            part2=Participant(
+                                depend=TeamParticipatesChallenge.objects.get(team=second_part[i],
+                                                                             challenge=self.challenge),
+                                depend_method='itself')
+                        )
+                    )
+                tmp_team = second_part[0]
+                for j in range(0,(len(teams)-1)):
+                    second_part[j] = second_part[j+1]
+                second_part[(len(teams)/2)-1] = first_part[(len(teams)/2)-1]
+                if (len(teams)/2) > 2:
+                    for j in reversed(range(2, len(teams))):
+                        first_part[j] = first_part[j-1]
+                if (len(teams)/2) > 1:
+                    first_part[1] = tmp_team
+
+        # return matches # list of ordered matches each match 2 participant (team)
+
+    def create_new_double_elimination(self, teams):
+        matches = []
+        bye = None  # TODO: replace with the right object
+        teams_length = len(teams)
+        power2 = 1
+        while teams_length > power2:
+            power2 *=2
+        while teams_length < power2:
+            teams.append(bye)
+
+        # first round : all teams participate in double elimination
+        cur_round_length = power2/2
+        start_round_index = 0
+        for i in range(cur_round_length):
+            matches.append(
+                Match(
+                    competition=self,
+                    part1=Participant(
+                        depend=TeamParticipatesChallenge.objects.get(team=teams[2*i],challenge=self.challenge),
+                        depend_method='itself'),
+                    part2=Participant(
+                        depend=TeamParticipatesChallenge.objects.get(team=teams[2*i+1],challenge=self.challenge),
+                        depend_method='itself')
+                )
+            )
+
+        #for second round:
+        start_round_index += cur_round_length
+        cur_round_length /= 2
+
+
+        while cur_round_length >= 1:
+            # first step: winners vs winners
+            for i in range(cur_round_length):
+                matches.append(Match(competition=self, part1=Participant(depend=matches[2 * i], depend_method='winner'),
+                                     part2=Participant(depend=matches[2 * i + 1], depend_method='winner')))
+            # second step: losers vs losers
+            second_step_index = start_round_index + cur_round_length
+            for i in range(cur_round_length):
+                matches.append(Match(competition=self, part1=Participant(depend=matches[2 * i],depend_method='loser'),
+                                     part2=Participant(depend=matches[2 * i + 1],depend_method='loser')))
+            # third step: losers vs losers of winners
+            for i in range(cur_round_length):
+                matches.append(Match(competition=self, part1=Participant(depend=matches[second_step_index + i], depend_method='winner'),
+                                     part2=Participant(depend=matches[(second_step_index - 1) - i], depend_method='loser')))
+            #
+            start_round_index += 3*cur_round_length
+            cur_round_length /= 2
+        matches.append(
+        Match(competition=self, part1=Participant(depend=matches[start_round_index - 1], depend_method='winner'),
+              part2=Participant(depend=matches[start_round_index - 2], depend_method='winner')))
+        matches.append(
+            Match(competition=self, part1=Participant(depend=matches[start_round_index - 1], depend_method='winner'),
+                  part2=Participant(depend=matches[start_round_index - 2], depend_method='winner')))
+        # return matches
 
 class Participant(models.Model):
     METHOD_CHOICES = (
         ('winner', _('Winner')),  # IF WINNER, CONTENT TYPE SHOULD BE MATCH
         ('loser', _('Loser')),  # IF LOSER, CONTENT TYPE SHOULD BE MATCH
-        ('itself', _('Itself'))  # IF ITSELF, CONTENT TYPE SHOULD BE SUBMISSION
+        ('itself', _('Itself'))  # IF ITSELF, CONTENT TYPE SHOULD BE TeamParticipantsChallenge
     )
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)  # Match or None
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)  # Match or TeamParticipantsChallenge
     object_id = models.PositiveIntegerField(null=True)
     depend = GenericForeignKey()
     depend_method = models.CharField(max_length=128, choices=METHOD_CHOICES)
@@ -37,9 +139,6 @@ class Participant(models.Model):
     def update_depend(self):
         if self.submission is not None or self.depend is None:
             return
-        func = getattr(self.depend, self.depend_method)
-        self.submission = func()
-
 
 class Match(models.Model):
     competition = models.ForeignKey(Competition)
