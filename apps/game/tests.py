964cde11a48d8ed5a357d75812a5e63eb9ad7a4f
ehsan <soltanaghaei97@gmail.com>
2018-01-11 06:36:27 +0330

    game scheduling and test

--- apps/game/tests.py
+++ apps/game/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.accounts import tests
+from apps.accounts.models import Team
+from apps.game.models import Challenge, Competition
+
+
+class TestScheduling(TestCase):
+    def setUp(self):
+        super().setUp()
+        tests.populate_users()
+        tests.populate_teams()
+        tests.populate_challenges()
+        tests.populate_competitions()
+
+    def test_create_new_league(self):
+        competition = Competition(teams=Team.objects.all(), challenge=Challenge.objects.all()[0], type= 'league')
+        competition.save()
+        pass
