fb040419a884c438c990f4fba6220919e069dd23
ehsan <soltanaghaei97@gmail.com>
2018-01-11 18:52:42 +0330

    fix scheduling bugs and test accepted

--- apps/game/tests.py
+++ apps/game/tests.py
@@ -20,8 +20,9 @@ class TestScheduling(TestCase):
             participation.team = team
             participation.challenge = challenge
             participation.save()
-        competition = Competition(teams=Team.objects.all(), challenge=challenge, type='league')
+        competition = Competition(challenge=challenge, type='league')
         competition.save()
+        competition.create_new_league(teams=Team.objects.all())
 
         # expected result
         # 1 -> 3
@@ -62,3 +63,40 @@ class TestScheduling(TestCase):
         self.assertEqual(matches[10].part1.object_id, 2)
         self.assertEqual(matches[11].part2.object_id, None)
         self.assertEqual(matches[11].part1.object_id, 3)
+
+    def test_create_new_double_elimination(self):
+        challenge = Challenge.objects.all()[0]
+        for team in Team.objects.all():
+            participation = TeamParticipatesChallenge()
+            participation.team = team
+            participation.challenge = challenge
+            participation.save()
+
+        competition = Competition(challenge=challenge, type='double')
+        competition.save()
+        competition.create_new_double_elimination(teams=Team.objects.all())
+
+        # expected result
+        # 1 -> 2
+        # 3 -> None
+        # 1 -> 2
+        # 1 -> 2
+        # 4 -> 3
+        # 5 -> 4
+        # 5 -> 4
+        #
+        matches = list(Match.objects.all())
+        self.assertEqual(matches[0].part1.object_id, 1)
+        self.assertEqual(matches[0].part2.object_id, 2)
+        self.assertEqual(matches[1].part1.object_id, 3)
+        self.assertEqual(matches[1].part2.object_id, None)
+        self.assertEqual(matches[2].part1.object_id, 1)
+        self.assertEqual(matches[2].part2.object_id, 2)
+        self.assertEqual(matches[3].part1.object_id, 1)
+        self.assertEqual(matches[3].part2.object_id, 2)
+        self.assertEqual(matches[4].part1.object_id, 4)
+        self.assertEqual(matches[4].part2.object_id, 3)
+        self.assertEqual(matches[5].part1.object_id, 5)
+        self.assertEqual(matches[5].part2.object_id, 4)
+        self.assertEqual(matches[6].part1.object_id, 5)
+        self.assertEqual(matches[6].part2.object_id, 4)
