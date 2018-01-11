d7b5057a556d9e287f39e8a776b5d1933056575e
sadegh <smahdavi4@gmail.com>
2018-01-12 00:21:43 +0330

    fixed functions.py bugs and added tests for it

--- apps/game/functions.py
+++ apps/game/functions.py
@@ -1,5 +1,9 @@
 import random
 import string
+import threading
+import time
+
+from apps.game.models import TeamSubmission, Match
 
 
 def random_token():
@@ -23,15 +27,15 @@ def download_file(file_token):
     return "This is text will be replaced by the real log file"
 
 
-def compile_submissions(submission_tokens, game_id):
+def compile_submissions(file_tokens, game_id):
     # Tell the infrastructure to compile a list of submission
 
     submits = []
-    for submission_token in submission_tokens:
+    for file_token in file_tokens:
         submits.append({
             "game": game_id,
             "section": "compile",
-            "parameters": {
+            "parameters": {  # TODO : parameters
                 "string_parameter1": "parameter1_value",
                 "string_parameter2": "parameter2_value",
                 "file_parameter1": "file_parameter1_token"
@@ -39,16 +43,18 @@ def compile_submissions(submission_tokens, game_id):
         })
 
     # Send request to infrastructure to compile them
-    # TODO : A thread to call compile results
 
     compile_details = []  # Get the array from the infrastructure.
 
-    for x in submission_tokens:
-        compile_details.append({
+    for x in file_tokens:
+        gm = {
             "token": random_token(),
             "success": True,
             "errors": ""
-        })
+        }
+        compile_details.append(gm)
+        t = threading.Thread(target=compilation_result, args=(gm,))
+        t.start()
 
     return compile_details
 
@@ -62,7 +68,7 @@ def run_matches(matches, game_id):
         games.append({
             "game": game_id,
             "section": "play",
-            "parameters": {
+            "parameters": {  # TODO : parameters
                 "string_parameter1": "parameter1_value",
                 "string_parameter2": "parameter2_value",
                 "file_parameter1": "file_parameter1_token"
@@ -70,21 +76,25 @@ def run_matches(matches, game_id):
         })
 
     # Send request to infrastructure to compile them
-    # TODO : A thread to call matches results
 
     match_details = []  # Get the array from the infrastructure.
 
     for x in matches:
-        match_details.append({
+        gm = {
             "token": random_token(),
             "success": True,
             "errors": ""
-        })
+        }
+        match_details.append(gm)
+        t = threading.Thread(target=match_results, args=(gm,))
+        t.start()
 
     return match_details
 
 
 def compilation_result(compile_result):
+    time.sleep(0.2)  # one second delay for testing ... (Database errors may occur
+
     # Returns compilation results.
 
     token = compile_result["token"]
@@ -92,16 +102,18 @@ def compilation_result(compile_result):
     errors = ""
     parameters = {}
 
-    if success is False:
-        errors = ""
+    if success is True:
+        errors = "ok"
     else:
         parameters = {}
+        errors = "Error occurred"  # TODO : fix errors with the infrastructure
 
-    # TODO : update result in model
-    pass
+    TeamSubmission.objects.filter(infra_token=token).update(infra_compile_message=errors)
 
 
 def match_results(match):
+    time.sleep(0.2)  # one second delay for testing ... (Database errors may occur
+
     # Return matches results.
 
     token = match["token"]
@@ -109,18 +121,17 @@ def match_results(match):
     errors = ""
     parameters = {}
 
-    if success is False:
-        errors = ""
+    if success is True:
+        errors = "ok"
     else:
         parameters = {}
+        errors = "Error occurred"  # TODO : fix errors with the infrastructure
 
-    # TODO : update result in model
-    pass
+    Match.objects.filter(infra_token=token).update(infra_match_message=errors)
 
 
 def is_compile_report(game):
-    # TODO
-    return True
+    return TeamSubmission.objects.filter(infra_token=game["token"]).exists()
 
 
 def pull_reports():
