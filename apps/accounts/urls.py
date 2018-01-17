fdb931f163a0310cf5807afa27881c61751010d5
amirHosseinS3 <ahns3.ahe@gmail.com>
2018-01-17 18:34:49 +0330

    urls added

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.TeamForm.as_view(), name='create team'),
+    url(r'^$', views.create_team, name='create team'),
 ]
