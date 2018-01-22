d597387008d68a4f1c31e1528419688c6b6f0396
Ali Asgari <altostratous@gmail.com>
2018-01-22 20:04:56 +0330

    New Intro Page (#22)
    * Applied chairman's instructions.
    
    * merged with dev
    
    * Working on intro
    
    * countup
    
    * Working on intro
    
    * working on intro
    
    * Make some changes to intro and add some translations
    
    * Intro page - Context Processor - Translations - Overriding Translations - New semantic UI build
    
    * Remove allowed hosts from base

--- aic_site/settings/base.py
+++ aic_site/settings/base.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'aic_site.context_processors.menu'
             ],
         },
     },
@@ -144,3 +145,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "static/root/")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+    os.path.join(BASE_DIR, 'locale_extra'),
+)
