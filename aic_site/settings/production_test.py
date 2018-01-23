db24ff6b735a11d610758603e3ff0fc1d73e25fd
faryabimm <faryabimohammadmahdi@gmail.com>
2018-01-23 09:26:36 +0330

    attempting to fix CI again (1)

--- aic_site/settings/production_test.py
+++ aic_site/settings/production_test.py
@@ -2,13 +2,13 @@ from .production import *
 
 
 DEBUG = True
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'HOST': 'aic_test_db',
-#         'PORT': 5432,
-#     }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'aic_test_db',
+        'PORT': 5432,
+    }
+}
