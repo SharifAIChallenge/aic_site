50ac3698d213bdb5185a839b3cdb79e4a3c17272
altostratous <altostratous@gmail.com>
2018-01-26 01:46:37 +0330

    Finalize create team

--- apps/accounts/forms/team_forms.py
+++ apps/accounts/forms/team_forms.py
@@ -1,13 +1,17 @@
 from django.forms import ModelForm, Form
 from django import forms
 from apps.accounts.models import Team, UserParticipatesOnTeam, User
+from apps.game.models import Challenge, TeamParticipatesChallenge
+from apps.game.models.challenge import UserAcceptsTeamInChallenge
+from django.utils.translation import ugettext_lazy as _
 
 
 class CreateTeamForm(Form):
     team_name = forms.CharField(max_length=30, required=True,
-                                help_text='Required,Choose a suitable name for your team')
+                                help_text=_('Required,Choose a suitable name for your team'))
     member1 = forms.CharField(max_length=30, required=False)
     member2 = forms.CharField(max_length=30, required=False)
+    challenge_id = forms.IntegerField(required=False, widget=forms.HiddenInput())
 
     def is_valid(self):
         valid = super(Form, self).is_valid()
@@ -17,16 +21,55 @@ class CreateTeamForm(Form):
             return valid
         team_name = self.cleaned_data['team_name']
         if Team.objects.filter(name__exact=team_name).exists():
-            self._errors['team_name'] = "This name is used by another team!"
+            self._errors['team_name'] = _("This name is used by another team!")
             return False
         if member1_email and member2_email and (member1_email == member2_email):
-            self.add_error(None, "You can't add same users to your team!")
+            self.add_error(None, _("You can't add same users to your team!"))
+            return False
+        if UserAcceptsTeamInChallenge.objects.filter(user__email__in=[self.user.email, member1_email, member2_email],
+                                                     team__challenge_id=self.cleaned_data['challenge_id']).count() > 0:
+            self.add_error(None, _("Some of users are already participated in the challenge!"))
             return False
         return True
 
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+
     def save(self, commit=True):
-        super().save()
+        member1_email = self.cleaned_data['member1']
+        member2_email = self.cleaned_data['member2']
+        team_name = self.cleaned_data.get('team_name')
+        team = Team(name=team_name)
+        if commit:
+            team.save()
+        challenge = Challenge.objects.get(id=self.cleaned_data['challenge_id'])
+        team_challenge = TeamParticipatesChallenge(team=team, challenge=challenge)
+        if commit:
+            team_challenge.save()
+
+        if member1_email and member2_email:
+            member1 = User.objects.get(email__exact=member1_email)
+            member2 = User.objects.get(email__exact=member2_email)
+            user_team0 = UserParticipatesOnTeam(team=team, user=self.user)
+            user_team1 = UserParticipatesOnTeam(team=team, user=member1)
+            user_team2 = UserParticipatesOnTeam(team=team, user=member2)
+            if commit:
+                user_team0.save()
+                user_team1.save()
+                user_team2.save()
+        elif (not member2_email) and member1_email:
+            member1 = User.objects.get(email__exact=member1_email)
+            user_team0 = UserParticipatesOnTeam(team=team, user=self.user)
+            user_team1 = UserParticipatesOnTeam(team=team, user=member1)
+            if commit:
+                user_team0.save()
+                user_team1.save()
+        elif (not member2_email) and (not member1_email):
+            user_team0 = UserParticipatesOnTeam(team=team, user=self.user)
+            if commit:
+                user_team0.save()
 
     class Meta:
         model = UserParticipatesOnTeam
-        fields = ('team_name', 'member1', 'member2', )
+        fields = ('team_name', 'member1', 'member2',)
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,23 +1,25 @@
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from apps.accounts.forms.team_forms import CreateTeamForm
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
+
+from apps.accounts.forms.team_forms import CreateTeamForm
 # Create your views here.
 from django.views.generic import FormView, RedirectView
 
 from apps.accounts.forms.forms import SignUpForm, UpdateProfileForm
 from apps.accounts.forms.panel import SubmissionForm
 from apps.accounts.models import Profile, Team, UserParticipatesOnTeam
-from apps.game.models import TeamParticipatesChallenge, TeamSubmission
+from apps.game.models import TeamSubmission
 import json
-from apps.game.models.challenge import TeamParticipatesChallenge
+from apps.game.models.challenge import TeamParticipatesChallenge, UserAcceptsTeamInChallenge
 from apps.game.models.challenge import Challenge
+
 
 class SignupView(generic.CreateView):
     form_class = SignUpForm
@@ -126,44 +128,15 @@ def reject_participation(request, participation_id):
     return redirect('accounts:panel')
 
 
-
 @login_required()
 def create_team(request, challenge_id):
     if request.method == 'POST':
-        form = CreateTeamForm(request.POST)
+        form = CreateTeamForm(request.user, request.POST)
         if form.is_valid():
-            member1_email = request.POST['member1']
-            member2_email = request.POST['member2']
-            team_name = form.cleaned_data.get('team_name')
-            team = Team(name=team_name)
-            team.save()
-            challenge = Challenge.objects.get(id=challenge_id)
-            team_challenge = TeamParticipatesChallenge(team=team, challenge=challenge)
-            team_challenge.save()
-
-            if member1_email and member2_email:
-                member1 = User.objects.get(email__exact=member1_email)
-                member2 = User.objects.get(email__exact=member2_email)
-                user_team0 = UserParticipatesOnTeam(team=team, user=request.user)
-                user_team0.save()
-                user_team1 = UserParticipatesOnTeam(team=team, user=member1)
-                user_team1.save()
-                user_team2 = UserParticipatesOnTeam(team=team, user=member2)
-                user_team2.save()
-                return redirect('accounts:success_create_team')
-            elif (not member2_email) and member1_email:
-                member1 = User.objects.get(email__exact=member1_email)
-                user_team0 = UserParticipatesOnTeam(team=team, user=request.user)
-                user_team0.save()
-                user_team1 = UserParticipatesOnTeam(team=team, user=member1)
-                user_team1.save()
-                return redirect('accounts:success_create_team')
-            elif (not member2_email) and (not member1_email):
-                user_team0 = UserParticipatesOnTeam(team=team, user=request.user)
-                user_team0.save()
-                return redirect('accounts:success_create_team')
+            form.save()
+            return redirect('accounts:success_create_team')
     else:
-        form = CreateTeamForm()
+        form = CreateTeamForm(user=request.user, initial={'challenge_id': challenge_id})
     return render(request, 'accounts/create_team.html', {'form': form
         , 'users': User.objects.exclude(username__exact=request.user.username)
         , 'username': request.user.username})
