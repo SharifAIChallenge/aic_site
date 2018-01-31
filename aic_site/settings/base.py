3ae616715926705a500c904213abad0ec7d058d3
Seyed Mohammad Sadegh Mahdavi <smahdavi4@gmail.com>
2018-01-31 18:43:03 +0330

    Update base.py

--- aic_site/settings/base.py
+++ aic_site/settings/base.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
 ]
 
 
