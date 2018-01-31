9c2edc70c724dcdcf1ba0984af4fcc348d77adc8
Ali Bahjati <bahjatia@gmail.com>
2018-01-31 03:38:58 +0330

    Add forgot password

--- aic_site/urls.py
+++ aic_site/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from apps.intro import urls as intro_urls
 from apps.accounts import urls as account_urls
@@ -24,6 +25,13 @@ from apps.game import urls as game_urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(intro_urls)),
+
+    url(r'^accounts/password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^accounts/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^accounts/reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
     url(r'^accounts/', include(account_urls)),
     url(r'^blog/', include(zinnia_urls)),
     url(r'^game/', include(game_urls)),
