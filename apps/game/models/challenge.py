9c5c4cd6d93dd3a1d7194848a2c935bc8971f605
Amin <maisaai1997@gmail.com>
2018-02-04 21:56:44 +0330

    Submit is open (#78)
    * is submission open for challenge
    
    * is submission open for challenge bug fixes
    
    * submisshion is open handeled
    
    * submission test ok

--- apps/game/models/challenge.py
+++ apps/game/models/challenge.py
@@ -28,6 +28,7 @@ class Challenge(models.Model):
     team_size = models.IntegerField()
     entrance_price = models.IntegerField()  # In Toomans, 0 for free
     game = models.ForeignKey(Game)
+    is_submission_open = models.BooleanField(null=False, blank=False,default=False)
 
     def __str__(self):
         return self.title
