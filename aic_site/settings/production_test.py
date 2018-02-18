c90ad248e35b2168bbe53cc3997407b3b05d8673
Ali Asgari <altostratous@gmail.com>
2018-02-18 08:10:59 +0330

    Fix schemaurl and url.

--- aic_site/settings/production_test.py
+++ aic_site/settings/production_test.py
@@ -18,3 +18,6 @@ EMAIL_HOST = 'smtp.mailgun.com'
 EMAIL_HOST_USER = 'info@aichallenge.ir'
 DEFAULT_FROM_EMAIL = 'info@aichallenge.ir'
 INFRA_IP = '144.202.6.232'
+INFRA_PORT = '8000'
+INFRA_URL = 'http://{}:{}'.format(INFRA_IP, INFRA_PORT)
+INFRA_API_SCHEMA_ADDRESS = "{}/api/schema/".format(INFRA_URL)
