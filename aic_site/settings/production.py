3e8f9b58af4392a12c1f45d82f76d00880bf8e0f
faryabimm <faryabimohammadmahdi@gmail.com>
2018-03-04 15:35:49 +0330

    reverting changes in settings files

--- aic_site/settings/production.py
+++ aic_site/settings/production.py
@@ -8,7 +8,7 @@ INSTALLED_APPS += [
 DEBUG = False
 
 ALLOWED_HOSTS = ['81.31.168.207', 'aichallenge.sharif.edu', 'aichallenge.sharif.ir', 'aichallenge.sharif.ac.ir']
-# CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailgun.com'
@@ -25,7 +25,7 @@ DATABASES = {
     }
 }
 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
