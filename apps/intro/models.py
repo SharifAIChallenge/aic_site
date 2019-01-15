40b73699ba7ccc12393a235e4ea1fdac6fd728cf
garjania <a.garjani@gmail.com>
2019-01-15 12:24:10 +0330

    staff back-end created

--- apps/intro/models.py
+++ apps/intro/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Staff(models.Model):
+    name = models.CharField(max_length=128, default="")
+    team = models.CharField(max_length=20, default="")
+
 # Create your models here.
--- apps/intro/views.py
+++ apps/intro/views.py
@@ -1,11 +1,13 @@
 import logging
 
+from PIL import Image
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
 
 from apps.accounts.models import Team
 from apps.game.models import TeamSubmission
+from apps.intro.models import Staff
 
 logger = logging.getLogger(__name__)
 
@@ -32,4 +34,29 @@ def not_found(request):
 
 
 def staffs(request):
-    return render(request, 'intro/staffs.html')
+    staff = Staff.objects.all()
+    if request.POST and str(request.POST.get('team')) != 'همه':
+        staff = Staff.objects.filter(team=request.POST.get('team'))
+    return render(request, 'intro/staffs.html', {
+        "staff": staff
+    })
+
+def add_staff(request):
+    if request.POST:
+        name = request.POST.get('name')
+        team = request.POST.get('team')
+        pic = request.FILES['photo']
+        handle_uploaded_file(pic, str(name) + '.jpg')
+        Staff.objects.create(name=name, team=team)
+    return render(request, 'intro/staff-form.html')
+
+
+def handle_uploaded_file(file, filename):
+
+    with open('apps/intro/static/staff_pic/' + filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+    image = Image.open('apps/intro/static/staff_pic/' + filename)
+    cropped_image = image.crop((0, 0, 100, 100))
+    resized_image = cropped_image.resize((100, 100), Image.ANTIALIAS)
+    resized_image.save('apps/intro/static/staff_pic/' + filename)
