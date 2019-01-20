06b39b06bc6b6fa10574d7d724c7d6a8353ce558
arghavan99 <a.rezvani99@yahoo.com>
2019-01-20 11:43:10 +0330

    remove print

--- apps/modir/views.py
+++ apps/modir/views.py
@@ -6,5 +6,4 @@ from apps.modir.models import URLShortner
 
 def redirect_shortened_url(request, shortened):
     url = URLShortner.objects.get(shortened=shortened).main
-    print(url)
     return redirect(url)
