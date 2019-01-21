f52ecb00af387b863bd123fab3256c78f5dbeebb
arshia <arshiamoghimi77@gmail.com>
2019-01-21 20:25:58 +0330

    navbar items edited

--- aic_site/context_processors.py
+++ aic_site/context_processors.py
@@ -13,8 +13,8 @@ def menu(request):
                         _('Main Page'): reverse('intro:index'),
                         _('Introduction'): reverse('intro:index') + '#section-intro',
                         _('Prize'): reverse('intro:index') + '#section-prizes',
-                        _('FAQ'): reverse('intro:index') + "#section-faq",
                         _('Schedule'): reverse('intro:index') + '#section-schedule',
+                        _('FAQ'): reverse('intro:index') + "#section-faq",
                         _('Contact Us'): reverse('intro:index') + '#section-organizer',
                     }
                 },
