c5cffba01539701d1a056bac80fa0bd81bf33d49
garjania <a.garjani@gmail.com>
2019-01-22 21:43:14 +0330

    registered staff model

--- apps/intro/admin.py
+++ apps/intro/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Notification
+from .models import Notification, Staff
 
 # Register your models here.
 admin.site.register(Notification)
+admin.site.register(Staff)
