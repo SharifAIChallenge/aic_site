e9a9c7fee193853b0413aefeb3d5bda8a50df829
garjania <a.garjani@gmail.com>
2019-01-22 16:26:36 +0330

    staff page added

--- apps/intro/models.py
+++ apps/intro/models.py
@@ -6,4 +6,18 @@ class Notification(models.Model):
     def __str__(self):
         return str(self.email)
 
+class Staff(models.Model):
+    name = models.CharField(max_length=20, default="")
+    team = models.CharField(max_length=20, default="")
+    image = models.ImageField(null=True, upload_to='staff_pic')
+
+    @property
+    def color(self):
+        if self.team == 'executive':
+            return 'red'
+        elif self.team == 'site' or self.team == 'graphic' or self.team == 'infrastructure' or self.team == 'server and client':
+            return 'blue'
+        else:
+            return 'orange'
+
 # Create your models here.
