45bc07d7142560abd89d94497f09084d844f76c4
garjania <a.garjani@gmail.com>
2019-01-24 19:17:05 +0330

    staff pic hover on mobile fixed + branding and design team added + image crop and resize fixed in staff-form

--- apps/intro/form.py
+++ apps/intro/form.py
@@ -2,5 +2,5 @@ from django import forms
 
 class StaffForm(forms.Form):
     name = forms.CharField(max_length=128)
-    team = forms.ChoiceField(choices=(('executive','executive'),('site', 'site'),('graphic', 'graphic'), ('game design', 'game design'),('infrastructure','infrastructure'),('test','test'),('content','content'),('server and client','server and client')))
+    team = forms.ChoiceField(choices=(('executive','executive'),('site', 'site'),('graphic', 'graphic'), ('game design', 'game design'),('infrastructure','infrastructure'),('test','test'),('content','content'),('server and client','server and client'),('branding','branding'),('design','design')))
     image = forms.FileField()
