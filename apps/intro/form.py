2109a6955c3001b599901b114e4729b1a627fda1
hamidreza <hamidreza004@gmail.com>
2019-02-05 05:12:31 +0330

    resolve bug in staffs

--- apps/intro/form.py
+++ apps/intro/form.py
@@ -6,5 +6,6 @@ class StaffForm(forms.Form):
     team = forms.ChoiceField(choices=(('Site', 'Site'), ('Graphic', 'Graphic'),
                                       ('Game Design', 'Game Design'), ('Head', 'Head'),
                                       ('Infrastructure', 'Infrastructure'), ('Content', 'Content'),
-                                      ('Server and Client', 'Server and Client'), ('Others', 'Others')))
+                                      ('Server and Client', 'Server and Client'), ('Others', 'Others'),
+                                      ('Branding', 'Branding'), ('Design', 'Design')))
     image = forms.FileField()
